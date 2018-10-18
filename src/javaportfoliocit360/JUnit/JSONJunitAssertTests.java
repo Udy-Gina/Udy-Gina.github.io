f3de47d3797b7c8744137f4bc645de3ed15c5e7e
Gina Udy <Gina Udy@10.0.0.7>
2018-10-18 18:52:06 -0400

    JSON examples completed

--- src/javaportfoliocit360/JUnit/JSONJunitAssertTests.java
+++ src/javaportfoliocit360/JUnit/JSONJunitAssertTests.java
@@ -34,28 +34,4 @@ public class JSONJunitAssertTests {
         String[] methodOutput = {"apple", "mango", "grape"};
         assertArrayEquals(expectedOutput, methodOutput);
     }
-    
-    public class MyAssertEqualsTest {
- 
-        @Test
-        public void mySimpleEqualsTest(){
-
-            String expectedName = "Nattu";
-            assertEquals(expectedName, Employee.getEmpNameWithHighestSalary());
-        }
-
-        @Test
-        public void myObjectEqualsTest(){
-
-            Employee expectedEmpObj = new Employee(1, "Nattu", 15000);
-            assertEquals(expectedEmpObj, Employee.getHighestPaidEmployee());
-        }
-}
-
-
-
-
-
-
-
 }
