41f410a239a35a3b4a1a1c2a83db757c90646b74
Gina Udy <Gina Udy@10.0.0.7>
2018-12-10 19:26:36 -0500

    Deleted some files; saved new JUnit files

---
+++ src/javaportfoliocit360/JUnit/JUnitTest.java
@@ -0,0 +1,13 @@
+package javaportfoliocit360.JUnit;
+
+/**
+ *
+ * @author Gina Udy
+ */
+public class JUnitTest {
+    
+    public int add(String s1, String s2) {
+        return 0;
+    }
+    
+}
